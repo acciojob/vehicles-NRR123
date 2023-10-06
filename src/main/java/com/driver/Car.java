15c39d977d3f6b006e1375f7cab3cf7d8b3b5133
unknown <you@example.com>
2023-10-06 08:52:14 +0530

    commit1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,6 +1,7 @@
 package com.driver;
 
 public class Car extends Vehicle {
+
     private int wheels;
     private String type;
     private int doors;
@@ -11,14 +12,53 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+
+        currentGear = 1;
     }
 
-    public void changeGear(int newGear){
+    public int getWheels() {
+        return wheels;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void changeGear(int newGear) {
+        currentGear = newGear;
 
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
-    public void changeSpeed(int newSpeed, int newDirection){
+    public void changeSpeed(int newSpeed, int newDirection) {
+        move(newSpeed, newDirection);
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
